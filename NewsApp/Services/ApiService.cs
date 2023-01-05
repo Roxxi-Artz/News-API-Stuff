dea3b6f38421dc5ec7f052977ba453fa6e6109f6
Roxanne Poynter <ccpoynter03@gmail.com>
2023-01-05 09:02:07 -0500

    updated

---
+++ NewsApp/Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using NewsApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Services
+{
+    public class ApiService
+    {
+        public async Task<Root> GetNews()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=b982d51a048be605486f758ab3da580e&topic=breaking-news&lang=en&topic=sports");
+
+            //deserialization
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
